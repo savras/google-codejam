3b7569f05e690e8667a11382a1db6dc264f56039
Vincent Gim <vincegim@amazon.com>
2021-08-21 15:41:51 -0700

    2021 RoundA RabbitHouse

---
+++ 2021/RabbitHouse/Solution.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+        int t = in.nextInt();
+
+        for (int i = 1; i <= t; ++i) {
+            System.out.println("Case #" + i + ": " + getResult(in));
+        }
+    }
+
+    private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    private static long getResult(Scanner in) {
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        int[][] arr = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        long result = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                result += dfs(arr, i, j);
+            }
+        }
+        return result;
+    }
+
+    private static long dfs(int[][] arr, int i, int j) {
+        int m = arr.length;
+        int n = arr[0].length;
+
+        int neighbourMax = arr[i][j];
+        for(int[] dir: directions) {
+            int nx = i + dir[0];
+            int ny = j + dir[1];
+
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                neighbourMax = Math.max(neighbourMax, arr[nx][ny]);
+            }
+        }
+
+        long result = 0;
+        // Set to 1 less than max neighbour
+        if(arr[i][j] < neighbourMax) {
+            result += neighbourMax - 1 - arr[i][j];
+            arr[i][j] = neighbourMax - 1;
+        }
+
+        // Fix neighbours if needed
+        for(int[] dir: directions) {
+            int nx = i + dir[0];
+            int ny = j + dir[1];
+
+            if(nx >= 0 && nx < m && ny >= 0 && ny < n &&
+                    Math.abs(arr[nx][ny] - arr[i][j]) > 1) {
+                result += dfs(arr, nx, ny);
+            }
+        }
+        return result;
+    }
+}
--- GoogleCodejam/src/com/google/codejam/Solution.java
+++ GoogleCodejam/src/com/google/codejam/Solution.java
@@ -1,14 +1,38 @@
 package com.google.codejam;
 
-import com.google.codejam.twenty.one.round.a.LShapedPlots;
+import com.google.codejam.twenty.one.round.a.RabbitHouse;
 
 import java.io.FileNotFoundException;
 
 public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
-        // Leetcode submission, change scanner to:
-        // Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        LShapedPlots s = new LShapedPlots();
+        RabbitHouse s = new RabbitHouse();
         s.solve();
+
+        //
+        // Submission template:
+        //
+        /**
+        import java.util.*;
+        import java.io.*;
+        import java.lang.Math;
+
+        public class Solution {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+                int t = in.nextInt();
+
+                for (int i = 1; i <= t; ++i) {
+                    System.out.println("Case #" + i + ": " + getResult(in));
+                }
+            }
+
+            private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+            private static int getResult(Scanner in) {
+
+            }
+         */
     }
 }
